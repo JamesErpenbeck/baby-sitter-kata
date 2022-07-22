12d8c4115963dcfa4bb9582a836c401034d4e361
Jimmy Erpenbeck <jimerp821@gmail.com>
2022-07-22 12:35:01 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [startTime, setStartTime] = useState("");
+  const [bedTime, setBedTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [amountDue, setAmountDue] = useState(0);
+
+  const startTimeHandler = (event) => {
+    setStartTime(event.target.value);
+  };
+  const bedTimeHandler = (event) => {
+    setBedTime(event.target.value);
+  };
+  const endTimeHandler = (event) => {
+    setEndTime(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const start = parseFloat(startTime.split(":").join("."));
+    const end = parseFloat(endTime.split(":").join("."));
+    const bed = parseFloat(bedTime.split(":").join("."));
+
+    if (start < 17) {
+      setStartTime("");
+      alert("Please enter a start time at or after 5PM");
+    }
+
+    //Bedtime rules could use more AC, so I went with just a valid time (5PM - 4AM)
+    if (bed >= 5 && bed <= 16) {
+      setBedTime("");
+      alert("Bed time must be between 5PM and 4AM.");
+    }
+
+    //acceptable end time between 17 - 23 and 0 - 4. only invalid end times 5-16
+    if (end >= 5 && end <= 16) {
+      setEndTime("");
+      alert("Please enter a end time at or before 4AM");
+    }
+
+    let startToBedPay = 0;
+    let bedToMidnightPay = 0;
+    let midnightToEndPay = 0;
+
+    //If bedtime is between 12AM - 4AM
+    if (bed >= 0 && bed <= 4) {
+      startToBedPay = (23 - 17 + parseInt(bed)) * 12;
+    } else {
+      startToBedPay = parseInt(bed - start) * 12;
+    }
+
+    if (bed >= 0 && bed <= 4) {
+      //If bedtime is after 12AM then $8hr window is skipped
+    } else {
+      bedToMidnightPay = parseInt(24 - bed) * 8;
+    }
+
+    if (end <= 4 && end >= 0) {
+      midnightToEndPay = parseInt(end) * 16;
+    }
+
+    setAmountDue(startToBedPay + bedToMidnightPay + midnightToEndPay);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <form onSubmit={submitHandler}>
+        <div className="form-content">
+          <div className="form-field">
+            <div className="label">Start Time</div>
+            <input
+              className="time-input"
+              type="time"
+              value={startTime}
+              onChange={startTimeHandler}
+              data-testid="start-time"
+            ></input>
+          </div>
+          <div className="form-field">
+            <div className="label">Bed Time</div>
+            <input
+              className="time-input"
+              type="time"
+              value={bedTime}
+              onChange={bedTimeHandler}
+              data-testid="bed-time"
+            ></input>
+          </div>
+          <div className="form-field">
+            <div className="label">End Time</div>
+            <input
+              className="time-input"
+              type="time"
+              value={endTime}
+              onChange={endTimeHandler}
+              data-testid="end-time"
+            ></input>
+          </div>
+          <div className="button-container">
+            <button className="button" data-testid="submit-btn">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+      <div className="amount-due form-field">
+        <div className="label">Amount Due</div>
+        <div className="amount-due-card" data-testid="amount-due">
+          ${amountDue}
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,140 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("calculates amount due with start to bed ($12hr) and bed to end ($8hr) rates", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const startTime = screen.getByTestId("start-time");
+  expect(startTime).toBeInTheDocument();
+  fireEvent.change(startTime, { target: { value: "17:00" } });
+  expect(startTime).toHaveDisplayValue("17:00");
+
+  const bedTime = screen.getByTestId("bed-time");
+  expect(bedTime).toBeInTheDocument();
+  fireEvent.change(bedTime, { target: { value: "20:00" } });
+  expect(bedTime).toHaveDisplayValue("20:00");
+
+  const endTime = screen.getByTestId("end-time");
+  expect(endTime).toBeInTheDocument();
+  fireEvent.change(endTime, { target: { value: "22:00" } });
+  expect(endTime).toHaveDisplayValue("22:00");
+
+  const submit = screen.getByTestId("submit-btn");
+  expect(submit).toBeInTheDocument();
+  fireEvent.click(submit);
+
+  const amountDue = screen.getByTestId("amount-due");
+  expect(amountDue).toBeInTheDocument();
+  expect(amountDue).toHaveTextContent("$68");
+});
+
+test("calculates amount due start to bed, bed to midnight, and midnight to end rates", () => {
+  render(<App />);
+
+  const startTime = screen.getByTestId("start-time");
+  expect(startTime).toBeInTheDocument();
+  fireEvent.change(startTime, { target: { value: "17:00" } });
+  expect(startTime).toHaveDisplayValue("17:00");
+
+  const bedTime = screen.getByTestId("bed-time");
+  expect(bedTime).toBeInTheDocument();
+  fireEvent.change(bedTime, { target: { value: "20:00" } });
+  expect(bedTime).toHaveDisplayValue("20:00");
+
+  const endTime = screen.getByTestId("end-time");
+  expect(endTime).toBeInTheDocument();
+  fireEvent.change(endTime, { target: { value: "02:00" } });
+  expect(endTime).toHaveDisplayValue("02:00");
+
+  const submit = screen.getByTestId("submit-btn");
+  expect(submit).toBeInTheDocument();
+  fireEvent.click(submit);
+
+  const amountDue = screen.getByTestId("amount-due");
+  expect(amountDue).toBeInTheDocument();
+  expect(amountDue).toHaveTextContent("$100");
+});
+
+test("calculates amount due with bedtime after midnight", () => {
+  render(<App />);
+
+  const startTime = screen.getByTestId("start-time");
+  expect(startTime).toBeInTheDocument();
+  fireEvent.change(startTime, { target: { value: "17:00" } });
+  expect(startTime).toHaveDisplayValue("17:00");
+
+  const bedTime = screen.getByTestId("bed-time");
+  expect(bedTime).toBeInTheDocument();
+  fireEvent.change(bedTime, { target: { value: "01:00" } });
+  expect(bedTime).toHaveDisplayValue("01:00");
+
+  const endTime = screen.getByTestId("end-time");
+  expect(endTime).toBeInTheDocument();
+  fireEvent.change(endTime, { target: { value: "03:00" } });
+  expect(endTime).toHaveDisplayValue("03:00");
+
+  const submit = screen.getByTestId("submit-btn");
+  expect(submit).toBeInTheDocument();
+  fireEvent.click(submit);
+
+  const amountDue = screen.getByTestId("amount-due");
+  expect(amountDue).toBeInTheDocument();
+  expect(amountDue).toHaveTextContent("$132");
+});
+
+test("shows an alert if start time is before 5pm", () => {
+  global.alert = jest.fn();
+  jest.spyOn(global, "alert");
+  render(<App />);
+
+  const startTime = screen.getByTestId("start-time");
+  expect(startTime).toBeInTheDocument();
+  fireEvent.change(startTime, { target: { value: "16:00" } });
+
+  const submit = screen.getByTestId("submit-btn");
+  expect(submit).toBeInTheDocument();
+  fireEvent.click(submit);
+
+  expect(global.alert).toHaveBeenCalledTimes(1);
+  expect(global.alert).toHaveBeenCalledWith(
+    "Please enter a start time at or after 5PM"
+  );
+});
+
+test("shows an alert if bed time is not between 5PM and 4AM", () => {
+  global.alert = jest.fn();
+  jest.spyOn(global, "alert");
+  render(<App />);
+
+  const bedTime = screen.getByTestId("bed-time");
+  expect(bedTime).toBeInTheDocument();
+  fireEvent.change(bedTime, { target: { value: "05:00" } });
+
+  const submit = screen.getByTestId("submit-btn");
+  expect(submit).toBeInTheDocument();
+  fireEvent.click(submit);
+
+  expect(global.alert).toHaveBeenCalledTimes(1);
+  expect(global.alert).toHaveBeenCalledWith(
+    "Bed time must be between 5PM and 4AM."
+  );
+});
+
+test("shows an alert if end time is not between 5PM and 4AM", () => {
+  global.alert = jest.fn();
+  jest.spyOn(global, "alert");
+  render(<App />);
+
+  const endTime = screen.getByTestId("end-time");
+  expect(endTime).toBeInTheDocument();
+  fireEvent.change(endTime, { target: { value: "05:00" } });
+
+  const submit = screen.getByTestId("submit-btn");
+  expect(submit).toBeInTheDocument();
+  fireEvent.click(submit);
+
+  expect(global.alert).toHaveBeenCalledTimes(1);
+  expect(global.alert).toHaveBeenCalledWith(
+    "Please enter a end time at or before 4AM"
+  );
 });
